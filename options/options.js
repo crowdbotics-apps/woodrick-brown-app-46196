9171bc30a52592169437877cb8ef5e6caa10af50
Crowdbotics <team@crowdbotics.com>
2024-02-09 20:19:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "woodrick-brown-app-46196",
+  url: "https://woodrick-brown-app-46196.botics.co",
+  api: "https://woodrick-brown-app-46196.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
